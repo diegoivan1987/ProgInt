333f3710ffa0cfd174ab90f78e9839e6c5bf4580
Diego <diegoivan1987@outlook.com>
2023-10-31 10:46:12 -0600

    primera version del servidor

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
+from fastapi import FastAPI, HTTPException
 import tensorflow as tf
 import pickle
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+app = FastAPI()
 
 # Cargar el modelo previamente entrenado
 model = tf.keras.models.load_model("suicide_classifier_model.h5")
@@ -10,19 +13,17 @@ model = tf.keras.models.load_model("suicide_classifier_model.h5")
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-def evaluate_text(text):
+@app.post("/evaluate/")
+async def evaluate_text(text: str):
     # Procesamos el texto de entrada
     sequence = tokenizer.texts_to_sequences([text])
-    padded_sequence = pad_sequences(sequence, maxlen=240, padding='post') 
+    padded_sequence = pad_sequences(sequence, maxlen=240, padding='post')  # Asumiendo que 240 es la longitud que usaste al entrenar
+
+    # Evaluamos el texto con el modelo
     prediction = model.predict(padded_sequence)
 
     # Devolvemos el resultado
     if prediction[0][0] > 0.5:
-        return "Si"
+        return {"result": "El texto ingresado tiene una connotación relacionada con el suicidio."}
     else:
-        return "No"
-
-if __name__ == "__main__":
-    user_input = input("Por favor, ingrese un texto de prueba: ")
-    result = evaluate_text(user_input)
-    print(result)
+        return {"result": "El texto ingresado no tiene una connotación relacionada con el suicidio."}
